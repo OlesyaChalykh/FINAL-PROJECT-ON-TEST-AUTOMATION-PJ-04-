c3a386373fec0a283b0e460c66980fea349f710c
OlesyaChalykh <124625557+OlesyaChalykh@users.noreply.github.com>
2023-04-25 14:42:00 +0300

    Update and rename Registr.py to Email.py

--- pages/Email.py
+++ pages/Email.py
@@ -2,15 +2,14 @@ import json
 import requests
 
 
-class RegEmail:
-    """ Открываем сайт 1secmail.com, делаем запрос на создание виртуального email для регистрации на
-    https://b2c.passport.rt.ru/."""
+class Email:
+    # Открываем сайт 1secmail.com, делаем запрос на создание виртуального email для регистрации на https://b2c.passport.rt.ru/.
 
     def __init__(self):
-        self.base_url = 'https://www.1secmail.com/api/v1/'
+        self.base_url = "https://www.1secmail.com/api/v1/"
 
     def get_api_email(self) -> json:
-        """ Получаем случайный адрес электронной почты"""
+        # Получаем случайный адрес электронной почты
         action = {'action': 'genRandomMailbox', 'count': 1}
         res = requests.get(self.base_url, params=action)
         status_email = res.status_code
@@ -21,7 +20,7 @@ class RegEmail:
         return result_email, status_email
 
     def get_id_letter(self, login: str, domain: str) -> json:
-        """ Проверяем mailbox, получаем mail_id"""
+        # Проверяем mailbox
         action = {'action': 'getMessages', 'login': login, 'domain': domain}
         res = requests.get(self.base_url, params=action)
         status_id = res.status_code
@@ -32,7 +31,7 @@ class RegEmail:
         return result_id, status_id
 
     def get_reg_code(self, login: str, domain: str, ids: str) -> json:
-        """ Получаем письмо с кодом регистрации от Ростелекома (id=ids) """
+        # Получаем письмо с кодом регистрации от Ростелекома 
         action = {'action': 'readMessage', 'login': login, 'domain': domain, 'id': ids}
         res = requests.get(self.base_url, params=action)
         status_code = res.status_code
