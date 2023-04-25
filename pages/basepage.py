4820af2055a1fe8ff62ce819ca0e16da0fdae72d
OlesyaChalykh <124625557+OlesyaChalykh@users.noreply.github.com>
2023-04-25 15:55:34 +0300

    Create basepage.py

---
+++ pages/basepage.py
@@ -0,0 +1,5 @@
+from urllib.parse import urlparse
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from pages.settings import base_url
+from pages.locators import MainPageLocators
