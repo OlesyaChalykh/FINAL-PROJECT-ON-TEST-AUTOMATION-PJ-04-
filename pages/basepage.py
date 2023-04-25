f467138065d4b0c6d3c226632a6617d47c6384b2
OlesyaChalykh <124625557+OlesyaChalykh@users.noreply.github.com>
2023-04-25 21:07:56 +0300

    Delete basepage.py

--- pages/basepage.py
+++
@@ -1,5 +0,0 @@
-from urllib.parse import urlparse
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from pages.settings import base_url
-from pages.locators import MainPageLocators
